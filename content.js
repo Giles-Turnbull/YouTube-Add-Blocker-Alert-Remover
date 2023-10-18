ac62531335b8c7f35b98a329d7d2c2c03791a78f
Giles-Turnbull <71279574+Giles-Turnbull@users.noreply.github.com>
2023-10-18 18:51:20 +0100

    Play video

--- content.js
+++ content.js
@@ -1,10 +1,18 @@
 function removeDiv() {
-    var divToRemove = document.querySelector('tp-yt-paper-dialog[style-target="host"]');
-    if (divToRemove) {
-      divToRemove.remove();
-    }
+  var divToRemove = document.querySelector('tp-yt-paper-dialog[style-target="host"]');
+  if (divToRemove) {
+    // Remove the element
+    divToRemove.remove();
+
+    // Play the video after a short delay (e.g., 100 milliseconds)
+    setTimeout(function() {
+      var video = document.querySelector('video');
+      if (video && video.paused) {
+        video.play();
+      }
+    }, 100);
   }
-  
-  // Execute the function when the page is fully loaded.
-  window.addEventListener('load', removeDiv);
-  
+}
+
+// Execute the function when the page is fully loaded.
+window.addEventListener('load', removeDiv);
